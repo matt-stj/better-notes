bd031996bb53a416806f3481f4e185af356dbfe4
Matt Stjernholm <thew88@gmail.com>
2016-02-22 14:58:09 -0700

    implement createRecord

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -8,6 +8,15 @@ export default DS.Adapter.extend({
 
   findAll() {
     return filesystem.all();
+  },
+
+  createRecord(store, type, snapshot) {
+    var data = this.serialize(snapshot, { includeId: true });
+    var filename = data.id
+    var content = data.content
+
+    return filesystem.write(filename, content)
+       .then(file => console.log(file));
   }
 
 });
---
+++ app/controllers/notes.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+
+export default Ember.Controller.extend({
+  newNoteId: null,
+  newNoteContent: null,
+
+  actions: {
+    createNewNote: function(){
+      let id = this.get('newNoteId');
+      let content = this.get('newNoteContent');
+      
+      this.store.createRecord('note', {
+        id: id,
+        content: content
+      }).save();
+    }
+  }
+
+});

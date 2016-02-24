4de109e46c4efae80cfdbc00b2c3ccf8a656042a
Matt Rooney <matthewjrooney@gmail.com>
2016-02-24 15:15:08 -0700

    removed renderedNote from component and replace with if statement

--- app/components/html-renderer.js
+++ app/components/html-renderer.js
@@ -4,9 +4,9 @@ export default Ember.Component.extend({
 
   showRendered: false,
 
-  renderedNote: Ember.computed('note.content', 'showRendered', function () {
-    var content = '<h4>' + this.get('note.content').toUpperCase() + '</h4>';
-    return this.get('showRendered') ? content : Ember.Handlebars.SafeString(content);
-  })
+  // renderedNote: Ember.computed('note.content', 'showRendered', function () {
+  //   var content = this.get('note.content');
+  //   return this.get('showRendered') ? content : Ember.Handlebars.SafeString(content);
+  // })
 
 });

6221d41b502fa39720fbf197380de2fbfeb61ae1
Matt Stjernholm <thew88@gmail.com>
2016-02-24 10:43:59 -0700

    fix word count

--- app/components/note-counter.js
+++ app/components/note-counter.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   wordCount:  Ember.computed('note.content', function(){
-    return this.get('note.content').split(' ').length;
+    let content = this.get('note.content')
+    return content.length ? content.split(' ').length : 0;
   }),
 
   characterCount: Ember.computed('note.content', function(){

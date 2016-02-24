b707c1e0f1263dd90e8941ba02f65d56a2098598
Matt Stjernholm <thew88@gmail.com>
2016-02-24 13:24:14 -0700

    all functionality from steve's lesson working

---
+++ app/components/html-renderer.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  showRendered: false,
+
+  renderedNote: Ember.computed('note.content', 'showRendered', function () {
+    var content = '<h4>' + this.get('note.content').toUpperCase() + '</h4>';
+    return this.get('showRendered') ? Ember.Handlebars.SafeString(content) : content;
+  })
+
+});
